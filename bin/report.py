4c217910a4bcee2fa850b8764298900fa490ecb2
crosenth <crosenth@uw.edu>
2020-04-26 17:34:36 -0700

    concats CSVs into single report

--- bin/report.py
+++ bin/report.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
+import itertools
+import operator
 import sys
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('arg1')
-    parser.add_argument('--option1')
+    parser.add_argument('columns')
+    parser.add_argument('csvs', nargs='+', type=argparse.FileType('r'))
+    parser.add_argument('--sortby')
     parser.add_argument(
         '--out', type=argparse.FileType('w'), default=sys.stdout)
     return parser.parse_args()
@@ -15,7 +19,21 @@ def get_args():
 
 def main():
     args = get_args()
-    print(args)
+    rows = []
+    csvs = (r for f in args.csvs for r in csv.DictReader(f))
+    csvs = sorted(csvs, key=operator.itemgetter('sample'))
+    csvs = itertools.groupby(csvs, key=operator.itemgetter('sample'))
+    for sample, columns in csvs:
+        row = {'sample': sample}
+        for c in columns:
+            row.update(c)
+        rows.append(row)
+    out = csv.DictWriter(
+        args.out,
+        extrasaction='ignore',
+        fieldnames=args.columns.split(','))
+    out.writeheader()
+    out.writerows(rows)
 
 
 if __name__ == '__main__':
